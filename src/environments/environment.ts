379e138db20aff396f2aa14d677e337c4be05a08
Adam Marcinkowski <a.marcinkowski@macopedia.pl>
2018-03-03 10:32:08 +0100

    init repo

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+    firebase: {
+        apiKey: "AIzaSyDtr1AqSuIvncpkUAqPBVqP1hGCOJ9Q2N4",
+        authDomain: "chat-496e4.firebaseapp.com",
+        databaseURL: "https://chat-496e4.firebaseio.com",
+        projectId: "chat-496e4",
+        storageBucket: "",
+        messagingSenderId: "1035748357915"
+    }
+};
